984c3ab65e9259e2477c00077cc9c4f7b9453f0d
Yohann Legrand <yohann.legrand-ext@believe.com>
2022-07-06 11:14:52 +0200

    responsive headers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react"
+import styled from "styled-components/macro"
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
+import { COLORS, WEIGHTS } from "../../constants"
+import Logo from "../Logo"
+import SuperHeader from "../SuperHeader"
+import MobileMenu from "../MobileMenu"
+import UnstyledButton from "../UnstyledButton"
+import Icon from "../Icon"
 
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+  const [showMobileMenu, setShowMobileMenu] = React.useState(false)
 
   // For our mobile hamburger menu, we'll want to use a button
   // with an onClick handler, something like this:
@@ -30,6 +32,17 @@ const Header = () => {
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
         <Side />
+        <MobileNav>
+          <UnstyledButton>
+            <Icon id="shopping-bag" strokeWidth={2} />
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="search" strokeWidth={2} />
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="menu" strokeWidth={2} />
+          </UnstyledButton>
+        </MobileNav>
       </MainHeader>
 
       <MobileMenu
@@ -37,8 +50,8 @@ const Header = () => {
         onDismiss={() => setShowMobileMenu(false)}
       />
     </header>
-  );
-};
+  )
+}
 
 const MainHeader = styled.div`
   display: flex;
@@ -46,17 +59,38 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
-`;
+
+  @media ${({ theme }) => theme.queries.phoneAndDown} {
+    padding: 18px;
+  }
+`
 
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
-`;
+
+  @media ${({ theme }) => theme.queries.tabletAndDown} {
+    display: none;
+  }
+`
+
+const MobileNav = styled.nav`
+  display: none;
+
+  @media ${({ theme }) => theme.queries.tabletAndDown} {
+    display: flex;
+    gap: 32px;
+  }
+
+  @media ${({ theme }) => theme.queries.phoneAndDown} {
+    gap: 16px;
+  }
+`
 
 const Side = styled.div`
   flex: 1;
-`;
+`
 
 const NavLink = styled.a`
   font-size: 1.125rem;
@@ -68,6 +102,6 @@ const NavLink = styled.a`
   &:first-of-type {
     color: ${COLORS.secondary};
   }
-`;
+`
 
-export default Header;
+export default Header
