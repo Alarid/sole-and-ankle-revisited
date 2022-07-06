1cd307d1a4b62654d00be5649e18cea54860310b
Yohann Legrand <yohann.legrand-ext@believe.com>
2022-07-06 14:33:26 +0200

    improve breakpoints readability

--- src/components/Theme/Theme.js
+++ src/components/Theme/Theme.js
@@ -2,16 +2,17 @@ import styled from "styled-components/macro"
 import { ThemeProvider as StyledThemeProvider } from "styled-components"
 
 const breakpoints = {
-  phone: 37.5,
-  tablet: 59.375,
-  laptop: 81.25,
+  phone: 600,
+  tablet: 950,
+  laptop: 1300,
 }
+
 const theme = {
   breakpoints,
   queries: {
-    phoneAndDown: `(max-width: ${breakpoints.phone}rem)`,
-    tabletAndDown: `(max-width: ${breakpoints.tablet}rem)`,
-    laptopAndDown: `(max-width: ${breakpoints.laptop}rem)`,
+    phoneAndDown: `(max-width: ${breakpoints.phone / 16}rem)`,
+    tabletAndDown: `(max-width: ${breakpoints.tablet / 16}rem)`,
+    laptopAndDown: `(max-width: ${breakpoints.laptop / 16}rem)`,
   },
 }
 
